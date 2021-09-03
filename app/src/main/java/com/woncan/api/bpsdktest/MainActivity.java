5a351dbc9667dbf55154209923464381b85e5c0b
刘照 <liuzhao@woncan.com>
2021-09-03 16:13:49 +0800

    first commit

---
+++ app/src/main/java/com/woncan/api/bpsdktest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.woncan.api.bpsdktest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(v -> {
+
+        });
+    }
+
+
+
+
+}
